3637d57c27129069acc1decdde68a0bc78ffd5a1
周亚楠 <zhouyanan@keepfun.cn>
2021-09-10 10:10:45 +0800

    提交framework

---
+++ GraceReaderFramework/GraceArtist.framework/Headers/GraceArtist.h
@@ -0,0 +1,18 @@
+//
+//  GraceArtist.h
+//  GraceArtist
+//
+//  Created by KeepFun on 2021/9/10.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GraceArtist.
+FOUNDATION_EXPORT double GraceArtistVersionNumber;
+
+//! Project version string for GraceArtist.
+FOUNDATION_EXPORT const unsigned char GraceArtistVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GraceArtist/PublicHeader.h>
+
+#import <GraceArtist/ReaderModel.h>
---
+++ GraceReaderFramework/GraceArtist.framework/Headers/ReaderModel.h
@@ -0,0 +1,16 @@
+//
+//  ReaderModel.h
+//  Reader
+//
+//  Created by KeepFun on 2021/9/9.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface ReaderModel : NSObject
+
++(NSString *)returnResult;
+
+@end
+
